da01a2443308b46927f5a196e137f644dde59731
Purple4pur <purple4pur@gmail.com>
2022-12-03 01:04:56 +0800

    dssq

---
+++ Makefile
@@ -0,0 +1,32 @@
+# default target
+target = cpp
+
+COMP = g++
+ifeq ($(target),c)
+  COMP = gcc
+endif
+
+.PHONY: all help run build gen clean
+all: help
+
+help:
+	@cat help.txt
+
+run: code.$(target).exe
+	./$^
+
+build: code.$(target).exe
+code.$(target).exe: code.$(target)
+	@echo "********************************************"
+	@echo "This compiling may consume more than 30 mins"
+	@echo "on low-spec computers. Good luck!"
+	@echo "********************************************"
+	$(COMP) $^ -o $@ -O0
+
+gen: code.$(target)
+code.$(target): gen.py
+	python $^ $(target) $(d)
+
+clean:
+	-rm -rf code.cpp code.cpp.exe
+	-rm -rf code.c code.c.exe
---
+++ gen.py
@@ -0,0 +1,90 @@
+import sys
+
+def gen_cpp(loop_num):
+    f = open("code.cpp", "w")
+    f.write("""#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "请给出一个不多于5位的正整数：";
+    int x;
+    cin >> x;
+
+    switch (x) {
+""")
+
+    for num in range(1, loop_num):
+        digits = []
+        tmp = num
+        while (tmp != 0):
+            digits.append(tmp % 10)
+            tmp //= 10
+        f.write("    case " + str(num) + ":\n")
+        f.write("        cout << \"是" + str(len(digits)) + "位数\" << endl;\n")
+        for i, d in enumerate(digits):
+            pos = ["个", "十", "百", "千", "万"][i]
+            f.write("        cout << \"" + pos + "位数是：" + str(d) + "\" << endl;\n")
+        f.write("        cout << \"倒过来是：")
+        for d in digits:
+            f.write(str(d))
+        f.write("\" << endl;\n")
+        f.write("        break;\n")
+
+    f.write("""    default:
+        cout << "数字不合规！" << endl;
+    }
+}
+""")
+    f.close()
+
+
+def gen_c(loop_num):
+    f = open("code.c", "w")
+    f.write("""#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("请给出一个不多于5位的正整数：");
+    scanf("%d", &x);
+
+    switch (x) {
+""")
+
+    for num in range(1, loop_num):
+        digits = []
+        tmp = num
+        while (tmp != 0):
+            digits.append(tmp % 10)
+            tmp //= 10
+        f.write("    case " + str(num) + ":\n")
+        f.write("        printf(\"是" + str(len(digits)) + "位数\\n\");\n")
+        for i, d in enumerate(digits):
+            pos = ["个", "十", "百", "千", "万"][i]
+            f.write("        printf(\"" + pos + "位数是：" + str(d) + "\\n\");\n")
+        f.write("        printf(\"倒过来是：")
+        for d in digits:
+            f.write(str(d))
+        f.write("\\n\");\n")
+        f.write("        break;\n")
+
+    f.write("""    default:
+        printf("数字不合规！\\n");
+    }
+}
+""")
+    f.close()
+
+
+def main():
+    if len(sys.argv) in (2, 3):
+        loop_num = int(sys.argv[2]) if len(sys.argv) == 3 else 100000
+        target = sys.argv[1]
+        if target == "c":
+            gen_c(loop_num)
+        elif target == "cpp":
+            gen_cpp(loop_num)
+
+if __name__ == "__main__":
+    main()
